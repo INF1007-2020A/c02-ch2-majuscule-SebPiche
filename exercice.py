a06cbd341e88cf5b9f539f99ac9f9d496287c140
SebPiche <breacham@hotmail.com>
2020-09-10 22:25:11 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -5,8 +5,7 @@ def majuscule(mot):
     for lettre in mot:
         # TODO completer la fonction ici
             distance_entre_min_et_maj = ord('a')-ord('A')
-        if ord('a)' <= ord(lettre) and ord('lettre') <= ord('z'):
-            lettre = ord(lettre)-distance_entre_min_et_maj
+            lettre = chr(ord(lettre)-distance_entre_min_et_maj
         resultat += lettre
     return resultat
 
