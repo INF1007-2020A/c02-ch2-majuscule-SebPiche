4c69a18ed2f7bb738c9dd64cfa8c02b7e2cb48da
SebPiche <breacham@hotmail.com>
2020-09-10 22:04:41 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,7 +6,7 @@ def majuscule(mot):
         # TODO completer la fonction ici
         if 65 <= ord(lettre) <= 90:
             lettre = chr(ord(lettre)+32)
-
+        
         resultat += lettre
     return resultat
 
