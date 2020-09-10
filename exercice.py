6d1389d0836f6a3e5182f0cd0b0b05fa5f379241
SebPiche <breacham@hotmail.com>
2020-09-10 22:00:27 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,8 +4,8 @@ def majuscule(mot):
     resultat = ''
     for lettre in mot:
         # TODO completer la fonction ici
-        ord(lettre)
-        
+        if 65 ord(lettre)
+
         resultat += lettre
     return resultat
 
